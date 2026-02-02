5f807a85e8d0a4b1aa557bb6e18a41f405872df2
для учебы <lab_user112@proton.me>
2026-02-02 15:32:14 +0500

    Create products.test.js

---
+++ services/products-service/tests/products.test.js
@@ -0,0 +1,8 @@
+const request = require('supertest');
+const app = require('../src/index');
+
+describe('Products Endpoints', () => {
+    it('should pass', async () => {
+        expect(true).toBe(true);
+    });
+});
