678b9731fd323623f59919ee977d5e838127f930
для учебы <lab_user112@proton.me>
2026-02-02 15:30:43 +0500

    Create orders.test.js

---
+++ services/orders-service/tests/orders.test.js
@@ -0,0 +1,8 @@
+const request = require('supertest');
+const app = require('../src/index');
+
+describe('Orders Endpoints', () => {
+    it('should pass', async () => {
+        expect(true).toBe(true);
+    });
+});
