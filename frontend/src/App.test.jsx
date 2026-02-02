629b6dc53b3fcf31348182bcbb77e149e8d47807
для учебы <lab_user112@proton.me>
2026-02-02 15:41:31 +0500

    Update App.test.jsx

--- frontend/src/App.test.jsx
+++ frontend/src/App.test.jsx
@@ -1,24 +1,24 @@
+import React from 'react';
 import { render, screen } from '@testing-library/react';
 import { BrowserRouter } from 'react-router-dom';
-import React from 'react';
 import Navbar from './components/Navbar';
 
 test('Навигация содержит логотип MODA', () => {
-    render(
-        <BrowserRouter>
-            <Navbar />
-        </BrowserRouter>
-    );
-    const logo = screen.getByText(/MODA/i);
-    expect(logo).toBeInTheDocument();
+  render(
+    <BrowserRouter>
+      <Navbar />
+    </BrowserRouter>
+  );
+  const logoElement = screen.getByText(/MODA/i);
+  expect(logoElement).toBeInTheDocument();
 });
 
-test('Навигация содержит ссылки каталога и корзины', () => {
-    render(
-        <BrowserRouter>
-            <Navbar />
-        </BrowserRouter>
-    );
-    expect(screen.getByText(/Каталог/i)).toBeInTheDocument();
-    expect(screen.getByText(/Корзина/i)).toBeInTheDocument();
+test('Навигация содержит ссылку на каталог', () => {
+  render(
+    <BrowserRouter>
+      <Navbar />
+    </BrowserRouter>
+  );
+  const catalogLink = screen.getByText(/Каталог/i);
+  expect(catalogLink).toBeInTheDocument();
 });
