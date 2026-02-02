82b8d223a07126846d6216e5ec4b14a80454cbd2
для учебы <lab_user112@proton.me>
2026-02-02 15:39:53 +0500

    Create jest.config.cjs

---
+++ frontend/jest.config.cjs
@@ -0,0 +1,11 @@
+module.exports = {
+  testEnvironment: 'jsdom',
+  transform: {
+    '^.+\\.(js|jsx)$': 'babel-jest',
+  },
+  moduleNameMapper: {
+    '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
+    '\\.(gif|ttf|eot|svg|png)$': '<rootDir>/__mocks__/fileMock.js'
+  },
+  setupFilesAfterEnv: ['<rootDir>/src/setupTests.js'],
+};
